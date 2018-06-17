29d3830d7f5a6f4e6a8b5751544fe0ff1280d700
Spencer E. Olson <olsonse@umich.edu>
2018-06-17 02:15:29 -0600

    add admin view for labdata

--- labenviron/admin.py
+++ labenviron/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class LabDataAdmin(admin.ModelAdmin):
+  #list_per_page = 25
+  list_display = ('host', 'time', 'temperature', 'pressure', 'humidity')
+  list_display_links = ('time', )
+  list_editable= ('host', )
+
+admin.site.register(LabData, LabDataAdmin)
