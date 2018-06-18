b23cec6b117a492aeace7ab8e594e20110619fee
Spencer E. Olson <olsonse@umich.edu>
2018-06-18 07:55:40 -0600

    ensure that the package dir is in python path

--- sensors/wsgi.py
+++ sensors/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+THIS_DIR = os.path.dirname(__file__)
+PACKAGE_DIR = os.path.join(THIS_DIR, os.path.pardir)
+sys.path.insert(0, PACKAGE_DIR)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensors.settings")
 
 from django.core.wsgi import get_wsgi_application
